a76f09007869f1d1fa7ea1355408f35c851a56fe
samkc-0 <sam.k.c.0@icloud.com>
2025-08-30 21:45:38 +0200

    add GetPostsForUser function

--- internal/database/models.go
+++ internal/database/models.go
@@ -28,6 +28,17 @@ type FeedFollow struct {
 	FeedID    uuid.UUID
 }
 
+type Post struct {
+	ID          uuid.UUID
+	CreatedAt   time.Time
+	UpdatedAt   sql.NullTime
+	Title       string
+	Url         string
+	Description string
+	PublishedAt sql.NullTime
+	FeedID      uuid.UUID
+}
+
 type User struct {
 	ID        uuid.UUID
 	Name      string
---
+++ sql/queries/posts.sql
@@ -0,0 +1,12 @@
+-- name: CreatePost :one
+insert into posts (
+  title, url, description, published_at, feed_id
+) values (
+  $1, $2, $3, $4, $5
+) returning *;
+
+-- name: GetPostsForUser :many
+select * from posts
+inner join feed_follows on
+  posts.feed_id = feed_follows.feed_id
+where feed_follows.user_id = $1;
