7601641c81c64e2beb25ff6b41785259f2bf8e1f
samkc-0 <sam.k.c.0@icloud.com>
2025-08-30 21:33:35 +0200

    create posts table

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+create table posts (
+  id uuid primary key,
+  created_at timestamp not null,
+  updated_at timestamp,
+  title text not null,
+  url text unique not null,
+  description text not null,
+  published_at timestamp,
+  feed_id uuid not null,
+  foreign key (feed_id) references feeds(id)
+);
+
+-- +goose Down
+drop table posts;
